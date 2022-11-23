4edd18a0d3e726ae2acff3653ad45b09ce2b1aa5
Camila-TS <camilathompson@gmail.com>
2022-11-23 13:06:39 -0300

    se separa array de contenido por cada elemento con salto de linea, se cambia texto y color al dar click en los botones segun el estado de la orden

--- src/components/Preparations.jsx
+++ src/components/Preparations.jsx
@@ -3,37 +3,36 @@ import styles from '../views/kitchen.module.css';
 
 const Preparations = ({item}) => {
 
-//tengo arreglo
-//quiero devolver cada item por separado
+  const [serve, setServe] = React.useState(true)
 
-    console.log(item.content)
-    const eachContent = item.content
-    console.log(eachContent)
-    // const eachArr = () => {
-    //     for (let i = 0; i < eachContent.length; i++) {
-    //        return eachContent[i] + <br />
-    //     }
-    // }
-    // console.log(eachArr)
-    
-        // const eachContent = item.content.forEach((element) => (
-        // <div key={item.id}>
-        // <span>{element}</span> <br />
-        // </div>
-        // )
-        // )
-        // console.log(eachContent) 
+  const itsReady = () => {
+    setServe(false)
+}
+  const notReady = () => {
+    setServe(true)
+  }
 
   return (
     <div className={styles.preparationItem}>
         <span id={styles.clientName}>{item.client}</span><hr />
         <span id={styles.date}>{item.date}</span><hr />
-        <span id={styles.content}>{item.content}</span><br />
+        <span id={styles.content}>
+          {item.content.map((element, index) => (
+            <span key={index}>
+              {element} <br />
+            </span>
+          )
+          )}
+        </span><br />
         <label id={styles.obsLabel}>Observación:</label>
         <span id={styles.obsContent}>{item.observation}</span><br />
         <div className={styles.buttonsWrapper}>
             <img id={styles.peso} src="https://i.ibb.co/31fcvVC/peso.png" alt="Pedir cuenta" />
-            <button>Pendiente</button>
+            {serve ? (
+            <button className={styles.pending} onClick={itsReady}>Pendiente</button>
+            ) : (
+              <button className={styles.ready} onClick={notReady}>Listo</button>
+            )}
             <img id={styles.edit} src="https://i.ibb.co/0rSxBJQ/editar.png" alt="Editar" />
         </div>
     </div>
