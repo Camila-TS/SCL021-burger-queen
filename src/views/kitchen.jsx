a522abbc483d3e7db38935eb4d767b2cf8d0341b
Camila-TS <camilathompson@gmail.com>
2022-11-24 17:48:06 -0300

    se agrega botón para tomar pedido en cocina, vista cocina responsive

--- src/views/kitchen.jsx
+++ src/views/kitchen.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import {
+  Link
+} from "react-router-dom";
 import { allOrders } from '../firebase'
 import styles from './kitchen.module.css';
 import Logo from '../components/Logo';
@@ -12,8 +15,19 @@ function Kitchen() {
   const myOrders = async () => {
     let ordersObtain = await allOrders()
     setList([...ordersObtain])
-    //console.log(ordersObtain)
+    //console.log(ordersObtain[0].date)
   }
+  
+  // const ordersByDate = () => {
+  //   for (let i = 0; i < item.length; i++) {
+  //     item[i].date.sort((a,b) => a-b)
+  //   }
+  // }
+  // console.log(item.date)
+
+  //const orderByDate = item.date.sort((a,b) => a-b)
+  //console.log(orderByDate)
+
 
   useEffect(() => {
     myOrders();
@@ -37,6 +51,7 @@ function Kitchen() {
         )
         )}
       </div>
+      <button className={styles.takeOrder}><Link className={styles.link} to="/">Tomar Pedido</Link></button>
     </div>
   )
 }
